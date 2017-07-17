6275824ec95bb54b8953a7c3c1b355c826bc2f0c
conblem <mail@conblem.me>
2017-07-18 00:39:43 +0200

    working on bulma support

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,19 @@ export default {
 </script>
 
 <style lang="sass">
+@import "~bulma/sass/utilities/initial-variables"
+@import "~bulma/sass/utilities/variables"
+@import "~bulma/sass/base/helpers"
+@import "~bulma/sass/utilities/mixins"
+@import "~bulma/sass/utilities/controls"
+
+
+@import "~bulma/sass/elements/content"
+@import "~bulma/sass/elements/image"
+@import "~bulma/sass/elements/button"
+
+@import "~bulma/sass/components/card"
+
 html, body
   width: 100%
   height: 100%
