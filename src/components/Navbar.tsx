82abc41ca36adbbe302f8826bb8d294b86b7c58a
conblem <mail@conblem.me>
2017-12-27 10:21:06 +0100

    Worked on Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,25 @@
 import { h } from "preact";
+import { Link } from "preact-router";
 
 export default () => (
   <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-      <a class="navbar-item" href="https://bulma.io">
-        <img
-          src="https://bulma.io/images/bulma-logo.png"
-          alt="Bulma: a modern CSS framework based on Flexbox"
-          width="112"
-          height="28"
-        />
-      </a>
+      <Link class="navbar-item" activeClassName="is-active" href="/">
+        thurcom.li
+      </Link>
+    </div>
+    <div class="navbar-menu is-active">
+      <Link class="navbar-item" activeClassName="is-active" href="/recordings">
+        Aufnahmen
+      </Link>
+      <Link class="navbar-item" activeClassName="is-active" href="/live">
+        Live
+      </Link>
+    </div>
+    <div class="navbar-end">
+      <Link class="navbar-item" activeClassName="is-active" href="/login">
+        Login
+      </Link>
     </div>
   </nav>
 );
